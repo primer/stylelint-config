e7a06a2a6548ca95bf5d805b07317bd9c3e6f77e
Shawn Allen <shawn.allen@github.com>
2019-03-25 22:57:14 -0700

    fix(no-override): match entire selectors, simplify

--- plugins/no-override.js
+++ plugins/no-override.js
@@ -3,32 +3,36 @@ const stylelint = require('stylelint')
 const ruleName = 'primer/no-override'
 const CLASS_PATTERN = /(\.[-\w]+)/g
 
+let primerMeta = {}
 let availableBundles = []
 try {
-  const meta = require('@primer/css/dist/meta.json')
-  availableBundles = Object.keys(meta.bundles)
+  primerMeta = require('@primer/css/dist/meta.json')
 } catch (error) {
   throw new Error(
     `[${ruleName}] Unable to require('@primer/css/dist/meta.json')! Did you install it as a peerDependency?`
   )
 }
 
+availableBundles = Object.keys(primerMeta.bundles)
+
 const messages = stylelint.utils.ruleMessages(ruleName, {
-  rejected: selector => `The class selector "${selector}" is immutable.`
+  rejected: selector => `The selector "${selector}" should not be overridden.`
 })
 
 module.exports = stylelint.createPlugin(ruleName, (enabled, options = {}) => {
   const {bundles = ['utilities']} = options
 
   const immutableSelectors = new Set()
+  const immutableClassSelectors = new Set()
   for (const bundle of bundles) {
     if (!availableBundles.includes(bundle)) {
       throw new Error(`Unknown @primer/css bundle: "${bundle}"!`)
     }
     const {cssstats} = require(`@primer/css/dist/${bundle}`)
     for (const selector of cssstats.selectors.values) {
+      immutableSelectors.add(selector)
       for (const classSelector of getClassSelectors(selector)) {
-        immutableSelectors.add(classSelector)
+        immutableClassSelectors.add(classSelector)
       }
     }
   }
@@ -45,23 +49,26 @@ module.exports = stylelint.createPlugin(ruleName, (enabled, options = {}) => {
       return
     }
 
+    const report = (rule, selector) => stylelint.utils.report({
+      message: messages.rejected(selector),
+      node: rule,
+      result,
+      ruleName
+    })
+
     root.walkRules(rule => {
-      for (const classSelector of getClassSelectors(rule.selector)) {
-        if (immutableSelectors.has(classSelector)) {
-          stylelint.utils.report({
-            message: messages.rejected(classSelector),
-            node: rule,
-            result,
-            ruleName
-          })
+      if (immutableSelectors.has(rule.selector)) {
+        report(rule, rule.selector)
+      } else {
+        for (const classSelector of getClassSelectors(rule.selector)) {
+          if (immutableClassSelectors.has(classSelector)) {
+            report(rule, classSelector)
+          }
         }
       }
     })
   }
 })
-
-module.exports.ruleName = ruleName
-module.exports.messages = messages
 
 function getClassSelectors(selector) {
   const match = selector.match(CLASS_PATTERN)
