415563bf3af1698c330ab6743cc488f7a7c1e6b3
Shawn Allen <shawn.allen@github.com>
2019-04-10 13:28:55 -0700

    test(no-override): add better tests, invalid "bundles" option

--- __tests__/no-override.js
+++ __tests__/no-override.js
@@ -1,4 +1,4 @@
-const {lint} = require('./utils')
+const {lint, extendDefaultConfig} = require('./utils')
 
 describe('primer/no-override', () => {
   it('reports instances of utility classes', () => {
@@ -15,6 +15,27 @@ describe('primer/no-override', () => {
       expect(data).toHaveErrored()
       expect(data).toHaveWarningsLength(1)
       expect(data).toHaveWarnings([`The selector "${selector}" should not be overridden. (primer/no-override)`])
+    })
+  })
+
+  it('reports instances of partial utility selectors', () => {
+    const selector = '.show-on-focus'
+    return lint(`.foo ${selector}:focus { color: #f00; }`).then(data => {
+      expect(data).toHaveErrored()
+      expect(data).toHaveWarningsLength(1)
+      expect(data).toHaveWarnings([`The selector "${selector}" should not be overridden. (primer/no-override)`])
+    })
+  })
+
+  it('warns when you pass an invalid bundle name', () => {
+    const config = extendDefaultConfig({
+      rules: {
+        'primer/no-override': [true, {bundles: ['asdf']}]
+      }
+    })
+    return lint('', config).then(data => {
+      expect(data).not.toHaveErrored()
+      expect(data.results[0].invalidOptionWarnings).toHaveLength(1)
     })
   })
 })
