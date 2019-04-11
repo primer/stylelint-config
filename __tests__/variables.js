4b9a398a0ba830a6cc6196981d9fe6c0ad81e1a7
Shawn Allen <shawn.allen@github.com>
2019-04-10 23:47:51 -0700

    chore: lint

--- __tests__/variables.js
+++ __tests__/variables.js
@@ -1,15 +1,16 @@
 const {lint, extendDefaultConfig} = require('./utils')
 
 describe('primer/variables', () => {
-
   describe('color properties', () => {
-
     it('reports color properties w/o variables', () => {
-      return lint(`.x { color: #f00; }`, extendDefaultConfig({
-        rules: {
-          'primer/variables': true
-        }
-      })).then(data => {
+      return lint(
+        `.x { color: #f00; }`,
+        extendDefaultConfig({
+          rules: {
+            'primer/variables': true
+          }
+        })
+      ).then(data => {
         expect(data).toHaveErrored()
         expect(data).toHaveWarningsLength(1)
         expect(data).toHaveWarnings([`Please use a color variable for "color" instead of "#f00". (primer/variables)`])
@@ -17,17 +18,17 @@ describe('primer/variables', () => {
     })
 
     it('does not report color properties with allowed values', () => {
-      return lint(`.x { background-color: transparent; }`, extendDefaultConfig({
-        rules: {
-          'primer/variables': true
-        }
-      })).then(data => {
+      return lint(
+        `.x { background-color: transparent; }`,
+        extendDefaultConfig({
+          rules: {
+            'primer/variables': true
+          }
+        })
+      ).then(data => {
         expect(data).not.toHaveErrored()
         expect(data).toHaveWarningsLength(0)
       })
     })
-
   })
-
 })
-
