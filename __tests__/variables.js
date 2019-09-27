e5e1f3b8156930a083beb32a475869b3a9156223
Shawn Allen <shawn.allen@github.com>
2019-09-27 16:31:01 -0700

    refactor: nix utils from variables tests

--- __tests__/variables.js
+++ __tests__/variables.js
@@ -1,93 +1,90 @@
-const {lint, extendDefaultConfig} = require('./utils')
+const stylelint = require('stylelint')
+const ruleName = 'primer/variables'
+const pluginPath = require.resolve('../plugins/variables')
 
-describe('primer/variables', () => {
+const configWithOptions = args => ({
+  plugins: [pluginPath],
+  rules: {
+    [ruleName]: args
+  }
+})
+
+describe(ruleName, () => {
   describe('color properties', () => {
     it(`doesn't run when disabled`, () => {
-      return lint(
-        `.x { color: #f00; }`,
-        extendDefaultConfig({
-          rules: {
-            'primer/variables': false
-          }
+      return stylelint
+        .lint({
+          code: `.x { color: #f00; }`,
+          config: configWithOptions(false)
         })
-      ).then(data => {
-        expect(data).not.toHaveErrored()
-        expect(data).toHaveWarningsLength(0)
-      })
+        .then(data => {
+          expect(data).not.toHaveErrored()
+          expect(data).toHaveWarningsLength(0)
+        })
     })
 
     it('reports color properties w/o variables', () => {
-      return lint(
-        `.x { color: #f00; }`,
-        extendDefaultConfig({
-          rules: {
-            'primer/variables': true
-          }
+      return stylelint
+        .lint({
+          code: `.x { color: #f00; }`,
+          config: configWithOptions(true)
         })
-      ).then(data => {
-        expect(data).toHaveErrored()
-        expect(data).toHaveWarningsLength(1)
-        expect(data).toHaveWarnings([`Please use a color variable for "color" instead of "#f00". (primer/variables)`])
-      })
+        .then(data => {
+          expect(data).toHaveErrored()
+          expect(data).toHaveWarningsLength(1)
+          expect(data).toHaveWarnings([`Please use a color variable for "color" instead of "#f00". (primer/variables)`])
+        })
     })
 
     it('does not report color properties with allowed values', () => {
-      return lint(
-        `.x { background-color: transparent; }`,
-        extendDefaultConfig({
-          rules: {
-            'primer/variables': true
-          }
+      return stylelint
+        .lint({
+          code: `.x { background-color: transparent; }`,
+          config: configWithOptions(true)
         })
-      ).then(data => {
-        expect(data).not.toHaveErrored()
-        expect(data).toHaveWarningsLength(0)
-      })
+        .then(data => {
+          expect(data).not.toHaveErrored()
+          expect(data).toHaveWarningsLength(0)
+        })
     })
 
     it('reports properties with invalid variable usage', () => {
-      return lint(
-        `.x { background-color: $red; }`,
-        extendDefaultConfig({
-          rules: {
-            'primer/variables': true
-          }
+      return stylelint
+        .lint({
+          code: `.x { background-color: $red; }`,
+          config: configWithOptions(true)
         })
-      ).then(data => {
-        expect(data).toHaveErrored()
-        expect(data).toHaveWarningsLength(1)
-        expect(data).toHaveWarnings([
-          `Please use a background color variable for "background-color" instead of "$red". (primer/variables)`
-        ])
-      })
+        .then(data => {
+          expect(data).toHaveErrored()
+          expect(data).toHaveWarningsLength(1)
+          expect(data).toHaveWarnings([
+            `Please use a background color variable for "background-color" instead of "$red". (primer/variables)`
+          ])
+        })
     })
 
     it('does not report properties with any variables (and no allowedVariables)', () => {
-      return lint(
-        `.x { color: $red; }`,
-        extendDefaultConfig({
-          rules: {
-            'primer/variables': true
-          }
+      return stylelint
+        .lint({
+          code: `.x { color: $red; }`,
+          config: configWithOptions(true)
         })
-      ).then(data => {
-        expect(data).not.toHaveErrored()
-        expect(data).toHaveWarningsLength(0)
-      })
+        .then(data => {
+          expect(data).not.toHaveErrored()
+          expect(data).toHaveWarningsLength(0)
+        })
     })
 
     it('does not report properties with valid variables', () => {
-      return lint(
-        `.x { background-color: $bg-red; }`,
-        extendDefaultConfig({
-          rules: {
-            'primer/variables': true
-          }
+      return stylelint
+        .lint({
+          code: `.x { background-color: $bg-red; }`,
+          config: configWithOptions(true)
         })
-      ).then(data => {
-        expect(data).not.toHaveErrored()
-        expect(data).toHaveWarningsLength(0)
-      })
+        .then(data => {
+          expect(data).not.toHaveErrored()
+          expect(data).toHaveWarningsLength(0)
+        })
     })
   })
 })
