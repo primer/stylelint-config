f2563c6b2743a56fcb7a2d99f85810b3279166f2
Shawn Allen <shawn.allen@github.com>
2019-10-02 00:16:41 -0700

    tidy up variable rules logic, config

--- plugins/lib/variable-rules.js
+++ plugins/lib/variable-rules.js
@@ -1,48 +1,51 @@
 const SPACER_VARS = ['$spacer-*']
-
 const ALLOWED_COLOR_VALUES = ['currentColor', 'inherit', 'none', 'transparent']
 const ALLOWED_SPACE_VALUES = ['0']
 
 module.exports = {
-  'background{,-color}': {
-    name: 'background color',
+  'background color': {
+    props: ['background', 'background-color'],
     values: ['$bg-*', 'transparent', 'none']
   },
   border: {
-    values: ['$border'],
+    props: 'border',
+    values: ['$border', 'none'],
     components: ['border-width', 'border-style', 'border-color']
   },
-  ['border{,-top,-right,-bottom,-left}-color']: {
-    name: 'border color',
+  'border color': {
+    props: 'border{,-top,-right,-bottom,-left}-color',
     values: ['$border-*'].concat(ALLOWED_COLOR_VALUES)
   },
-  ['border{,-top,-right,-bottom,-left}-style']: {
-    name: 'border style',
+  'border style': {
+    props: 'border{,-top,-right,-bottom,-left}-style',
     values: ['$border-style']
   },
-  color: {
-    name: 'color (text)',
+  'color (text)': {
+    props: 'color',
     values: ['$text-*'].concat(ALLOWED_COLOR_VALUES)
   },
-  [`margin{,-top,-right,-bottom,-left}`]: {
-    name: 'margin',
+  marign: {
+    props: `margin{,-top,-right,-bottom,-left}`,
     values: SPACER_VARS.concat(ALLOWED_SPACE_VALUES)
   },
-  [`padding{,-top,-right,-bottom,-left}`]: {
-    name: 'padding',
+  padding: {
+    props: `padding{,-top,-right,-bottom,-left}`,
     values: SPACER_VARS.concat(ALLOWED_SPACE_VALUES)
   },
-  [`border{,-top,-right,-bottom,-left}-width`]: {
-    name: 'border width',
+  'border width': {
+    props: `border{,-top,-right,-bottom,-left}-width`,
     values: ['$border-width*', '0', 'none']
   },
   'font-size': {
+    props: 'font-size',
     values: ['$h[1-6]', '$font-size-*', '1', '1em', 'inherit']
   },
   'font-weight': {
+    props: 'font-weight',
     values: ['$font-weight-*', 'inherit']
   },
   'line-height': {
+    props: 'line-height',
     values: ['$lh-*', '0', '1', '1em', 'inherit']
   }
 }
--- plugins/variables.js
+++ plugins/variables.js
@@ -4,20 +4,18 @@ const TapMap = require('tap-map')
 
 const ruleName = 'primer/variables'
 
-const DEFAULT_PROPS = require('./lib/variable-props')
+const DEFAULT_RULES = require('./lib/variable-rules')
 
 module.exports = stylelint.createPlugin(ruleName, (enabled, options = {}) => {
-  const {categories = DEFAULT_PROPS} = options
+  const rules = Object.assign({}, DEFAULT_RULES, options.rules)
   const cache = new TapMap()
-  const entries = Object.entries(categories).map(([propPattern, category]) =>
-    Object.assign(
-      {
-        match: minimatch.filter(propPattern)
-      },
-      {name: propPattern},
-      category
-    )
-  )
+  const entries = Object.entries(rules).map(([name, rule]) => {
+    if (!rule.props) {
+      console.warn(`[${ruleName}] rule "${name}" has no .props; the name will be used.`)
+      rule.props = name
+    }
+    return Object.assign({name, match: matchAny(rule.props)}, rule)
+  })
 
   return (root, result) => {
     if (enabled === false) {
@@ -31,17 +29,17 @@ module.exports = stylelint.createPlugin(ruleName, (enabled, options = {}) => {
     root.walkDecls(decl => check(decl))
 
     function check(decl, prop = decl.prop, value = decl.value) {
-      const category = cache.tap(prop, () => entries.find(entry => entry.match(prop)))
-      if (category) {
-        const {name} = category
+      const rule = cache.tap(prop, () => entries.find(entry => entry.match(prop)))
+      if (rule) {
+        const {name} = rule
         const tokens = splitTokens(value)
-        if (tokens.length > 1 && Array.isArray(category.components)) {
-          for (const [index, comp] of Object.entries(category.components)) {
+        if (tokens.length > 1 && Array.isArray(rule.components)) {
+          for (const [index, comp] of Object.entries(rule.components)) {
             check(decl, comp, tokens[index])
           }
         } else {
           for (const token of tokens) {
-            if (!hasValue(category, token)) {
+            if (!hasValue(rule, token)) {
               const message = `Please use a ${name} variable instead of "${value}"`
               stylelint.utils.report({
                 message: messages.rejected(`${message}.`),
@@ -57,11 +55,28 @@ module.exports = stylelint.createPlugin(ruleName, (enabled, options = {}) => {
   }
 })
 
-function hasValue(category, value) {
-  const {values, test = v => values.some(p => minimatch(v, p))} = category
+function hasValue(rule, value) {
+  const {values, test = v => values.some(p => minimatch(v, p))} = rule
   return test(value)
 }
 
+/**
+ * In order to recognize "components" of compound properties like
+ * border and font, we have to know how to split up a CSS property
+ * value string into its constituent parts. However, just splitting on
+ * spaces isn't good enough, for instance:
+ *
+ * ```css
+ * border: ($spacer-2 - 1) solid lighten($gray-300, 4%);
+ * ```
+ *
+ * Math expressions, functions, and nested instances of each are
+ * treated as singular "tokens", so the above value would yield:
+ *
+ * 1. "($spacer-2 - 1)"
+ * 2. "solid"
+ * 3. "lighten($gray-300, 4%)"
+ */
 function splitTokens(value) {
   const tokens = []
   let token = ''
@@ -92,4 +107,13 @@ function splitTokens(value) {
     tokens.push(token)
   }
   return tokens.map(token => token.replace(/,$/, ''))
+}
+
+function matchAny(pattern) {
+  return Array.isArray(pattern) ? filterSome(pattern) : minimatch.filter(pattern)
+}
+
+function filterSome(patterns) {
+  const filters = patterns.map(pattern => minimatch.filter(pattern))
+  return value => filters.some(filter => filter(value))
 }
