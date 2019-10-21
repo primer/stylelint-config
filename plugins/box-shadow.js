d2c00c2deb361871c46198c328944c566aaedcb9
Shawn Allen <shawn.allen@github.com>
2019-10-21 16:39:50 -0700

    remove all reverseAssignments() calls from rules

--- plugins/box-shadow.js
+++ plugins/box-shadow.js
@@ -4,12 +4,6 @@ module.exports = createVariableRule('primer/box-shadow', {
   'box shadow': {
     expects: 'a box-shadow variable',
     props: 'box-shadow',
-    values: ['$box-shadow*', 'none'],
-    replacements: {
-      '0 1px 1px rgba($black, 0.1)': '$box-shadow',
-      '0 1px 5px $black-fade-15': '$box-shadow-medium',
-      '0 1px 15px $black-fade-15': '$box-shadow-large',
-      '0 10px 50px rgba($black, 0.07)': '$box-shadow-extra-large'
-    }
+    values: ['$box-shadow*', 'none']
   }
 })
