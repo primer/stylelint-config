99f0933d3a8bcf9e37a11e8e9bad3435ef8bf3f0
Shawn Allen <shawn.allen@github.com>
2019-10-22 14:37:41 -0700

    remove $h*-size-mobile from font-size rules

--- plugins/typography.js
+++ plugins/typography.js
@@ -3,7 +3,7 @@ const {createVariableRule} = require('./lib/variable-rules')
 module.exports = createVariableRule('primer/typography', {
   'font-size': {
     expects: 'a font-size variable',
-    values: ['$h{00,0,1,2,3,4,5,6}-size', '$h{00,0,1,2,3,4,5,6}-size-mobile', '$font-size-*', '1', '1em', 'inherit']
+    values: ['$h{00,0,1,2,3,4,5,6}-size', '$font-size-*', '1', '1em', 'inherit']
   },
   'font-weight': {
     props: 'font-weight',
