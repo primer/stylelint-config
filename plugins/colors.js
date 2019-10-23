345f1d0ecd7209f2a5750a43bdb3f15e3aa45087
Shawn Allen <shawn.allen@github.com>
2019-10-23 13:45:44 -0700

    make primer/colors smarter about shorthand background values

--- plugins/colors.js
+++ plugins/colors.js
@@ -1,15 +1,20 @@
 const {createVariableRule} = require('./lib/variable-rules')
 
+const bgVars = ['$bg-*', '$tooltip-background-color']
+
 module.exports = createVariableRule('primer/colors', {
-  'background color': {
+  'background-color': {
     expects: 'a background color variable',
-    props: ['background{,-color}'],
-    values: ['$bg-*', 'transparent', 'none']
+    values: bgVars.concat('none', 'transparent')
+  },
+  background: {
+    expects: 'a background color variable',
+    values: bgVars.concat('none', 'transparent', 'top', 'right', 'bottom', 'left', 'center', '*px', 'url(*)')
   },
   'text color': {
     expects: 'a text color variable',
     props: 'color',
-    values: ['$text-*', 'inherit'],
+    values: ['$text-*', '$tooltip-text-color', 'inherit'],
     replacements: {
       '#fff': '$text-white',
       white: '$text-white',
