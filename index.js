800baac4ab7a2fb1f8e7ad36944e38c5182c958c
Jon Rohan <yes@jonrohan.codes>
2021-11-23 15:50:55 -0800

    Extending stylelint-config-standard and removing defaults

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const browsers = require('./browsers')
 const propertyOrder = require('./property-order')
 
 module.exports = {
+  extends: ['stylelint-config-standard'],
   plugins: [
     'stylelint-no-unsupported-browser-features',
     'stylelint-order',
@@ -19,30 +20,9 @@ module.exports = {
   ],
   rules: {
     'at-rule-disallowed-list': ['extend'],
-    'at-rule-name-case': 'lower',
-    'at-rule-name-space-after': 'always-single-line',
-    'at-rule-semicolon-newline-after': 'always',
-    'block-closing-brace-newline-after': 'always',
-    'block-closing-brace-newline-before': 'always-multi-line',
-    'block-closing-brace-space-before': 'always-single-line',
     'block-no-empty': true,
-    'block-opening-brace-newline-after': 'always-multi-line',
-    'block-opening-brace-space-after': 'always-single-line',
-    'block-opening-brace-space-before': 'always',
-    'color-hex-case': 'lower',
-    'color-hex-length': 'short',
     'color-named': 'never',
     'color-no-invalid-hex': true,
-    'comment-empty-line-before': [
-      'always',
-      {
-        except: ['first-nested'],
-        ignore: ['stylelint-commands']
-      }
-    ],
-    'comment-whitespace-inside': 'always',
-    'declaration-bang-space-after': 'never',
-    'declaration-bang-space-before': 'always',
     'declaration-block-no-duplicate-properties': [
       true,
       {
@@ -52,47 +32,17 @@ module.exports = {
     'declaration-block-no-shorthand-property-overrides': true,
     'order/properties-order': propertyOrder,
     'declaration-block-semicolon-newline-after': 'always',
-    'declaration-block-semicolon-space-before': 'never',
-    'declaration-block-single-line-max-declarations': 1,
-    'declaration-block-trailing-semicolon': 'always',
-    'declaration-colon-newline-after': 'always-multi-line',
-    'declaration-colon-space-after': 'always-single-line',
-    'declaration-colon-space-before': 'never',
     'declaration-property-value-disallowed-list': {
       '/^transition/': ['/all/'],
       '/^background/': ['http:', 'https:'],
       '/^border/': ['none'],
       '/.+/': ['initial']
     },
-    'font-family-name-quotes': 'always-where-recommended',
     'function-calc-no-unspaced-operator': true,
-    'function-comma-newline-after': 'always-multi-line',
-    'function-comma-space-after': 'always-single-line',
-    'function-comma-space-before': 'never',
     'function-linear-gradient-no-nonstandard-direction': true,
-    'function-max-empty-lines': 0,
-    'function-name-case': 'lower',
-    'function-parentheses-newline-inside': 'always-multi-line',
-    'function-parentheses-space-inside': 'never-single-line',
-    'function-url-quotes': 'always',
-    'function-whitespace-after': 'always',
-    'length-zero-no-unit': true,
-    'max-empty-lines': 1,
     'max-nesting-depth': 3,
-    'media-feature-colon-space-after': 'always',
-    'media-feature-colon-space-before': 'never',
-    'media-feature-parentheses-space-inside': 'never',
-    'media-feature-range-operator-space-after': 'always',
-    'media-feature-range-operator-space-before': 'always',
-    'media-query-list-comma-newline-after': 'always-multi-line',
-    'media-query-list-comma-space-after': 'always-single-line',
-    'media-query-list-comma-space-before': 'never',
     'no-duplicate-selectors': true,
-    'no-eol-whitespace': true,
     'no-extra-semicolons': true,
-    'no-missing-end-of-source-newline': true,
-    'number-leading-zero': 'always',
-    'number-no-trailing-zeros': true,
     'plugin/no-unsupported-browser-features': [
       true,
       {
@@ -105,42 +55,15 @@ module.exports = {
     // unused vars are not necessarily an error, since they may be referenced
     // in other projects
     'primer/no-unused-vars': [true, {severity: 'warning'}],
-    'property-case': 'lower',
-    'property-no-vendor-prefix': true,
-    'rule-empty-line-before': [
-      'always-multi-line',
-      {
-        except: ['first-nested'],
-        ignore: ['after-comment']
-      }
-    ],
     'scss/selector-no-redundant-nesting-selector': true,
-    'selector-attribute-brackets-space-inside': 'never',
-    'selector-attribute-operator-space-after': 'never',
-    'selector-attribute-operator-space-before': 'never',
-    'selector-combinator-space-after': 'always',
-    'selector-combinator-space-before': 'always',
-    'selector-list-comma-newline-after': 'always',
-    'selector-list-comma-space-before': 'never',
     'selector-max-compound-selectors': 3,
-    'selector-max-empty-lines': 0,
     'selector-max-id': 0,
     'selector-max-specificity': '0,4,0',
     'selector-max-type': 0,
     'selector-no-qualifying-type': true,
-    'selector-pseudo-class-case': 'lower',
-    'selector-pseudo-class-parentheses-space-inside': 'never',
-    'selector-pseudo-element-case': 'lower',
-    'selector-pseudo-element-colon-notation': 'double',
     'selector-pseudo-element-no-unknown': true,
-    'selector-type-case': 'lower',
-    'shorthand-property-no-redundant-values': true,
     'string-no-newline': true,
     'string-quotes': 'single',
-    'unit-case': 'lower',
-    'unit-no-unknown': true,
-    'value-list-comma-space-after': 'always-single-line',
-    'value-list-comma-space-before': 'never',
-    'value-no-vendor-prefix': true
+    'unit-no-unknown': true
   }
 }
