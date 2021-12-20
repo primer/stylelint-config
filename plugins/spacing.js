939ba4a9d92018661d4c9fce8c6ef625a15d4e61
Jon Rohan <yes@jonrohan.codes>
2021-12-20 15:06:12 -0800

    Refactoring primer/spacing plugin.

--- plugins/spacing.js
+++ plugins/spacing.js
@@ -1,31 +1,116 @@
-const anymatch = require('anymatch')
-const {createVariableRule} = require('./lib/variable-rules')
+const stylelint = require('stylelint')
+const declarationValueIndex = require('stylelint/lib/utils/declarationValueIndex')
+const valueParser = require('postcss-value-parser')
 
-const spacerVarPatterns = ['$spacer-*', '$em-spacer-*']
-const values = [...spacerVarPatterns, '0', 'auto', 'inherit']
+const spacerValues = {
+  '$spacer-1': '4px',
+  '$spacer-2': '8px',
+  '$spacer-3': '16px',
+  '$spacer-4': '24px',
+  '$spacer-5': '32px',
+  '$spacer-6': '40px',
+  '$spacer-7': '48px',
+  '$spacer-8': '64px',
+  '$spacer-9': '80px',
+  '$spacer-10': '96px',
+  '$spacer-11': '112px',
+  '$spacer-12': '128px',
+  '$em-spacer-1': '0.0625em',
+  '$em-spacer-2': '0.125em',
+  '$em-spacer-3': '0.25em',
+  '$em-spacer-4': '0.375em',
+  '$em-spacer-5': '0.5em',
+  '$em-spacer-6': '0.75em'
+}
 
-module.exports = createVariableRule(
-  'primer/spacing',
-  ({variables}) => {
-    const spacerVars = Object.keys(variables).filter(anymatch(spacerVarPatterns))
-    const negativeValues = spacerVarPatterns.map(p => `-${p}`)
-    const replacements = {}
-    for (const name of spacerVars) {
-      replacements[`-${variables[name].computed}`] = `-${name}`
+const ruleName = 'primer/spacing'
+const messages = stylelint.utils.ruleMessages(ruleName, {
+  rejected: (value, replacement) => {
+    if (replacement === null) {
+      return `Please use a primer spacer variable instead of '${value}'. Consult the primer docs for a suitable replacement. https://primer.style/css/support/spacing`
     }
-    return {
-      margin: {
-        expects: 'a spacer variable',
-        props: 'margin{,-top,-right,-bottom,-left}',
-        values: values.concat(negativeValues),
-        replacements
-      },
-      padding: {
-        expects: 'a non-negative spacer variable',
-        props: 'padding{,-top,-right,-bottom,-left}',
-        values
+
+    return `Please replace ${value} with spacing variable '${replacement}'.`
+  }
+})
+
+module.exports = stylelint.createPlugin(ruleName, (enabled, options = {}, context) => {
+  if (!enabled) {
+    return noop
+  }
+
+  const lintResult = (root, result) => {
+    root.walkDecls(/^(padding|margin)/, decl => {
+      const problems = []
+      let containsMath = false
+      const parsedValue = valueParser(decl.value).walk(declValue => {
+        // Only check word types. https://github.com/TrySound/postcss-value-parser#word
+        if (!['word', 'function'].includes(declValue.type)) {
+          return false
+        }
+
+        // Ignore values that are not numbers.
+        if (['0', 'auto', 'inherit', 'initial'].includes(declValue.value)) {
+          return false
+        }
+        // Remove leading negative sign, if any.
+        const cleanDeclValue = declValue.value.replace(/^-/g, '')
+
+        // If the a variable is found in the value, skip it.
+        if (Object.keys(spacerValues).some(variable => cleanDeclValue === variable)) {
+          return false
+        }
+
+        // For now we're going to ignore math.
+        if (['*', '+', '-', '/'].includes(declValue.value)) {
+          containsMath = true
+          return false
+        }
+
+        let valueMatch = null
+        if ((valueMatch = Object.keys(spacerValues).find(spacer => spacerValues[spacer] === cleanDeclValue))) {
+          if (context.fix) {
+            declValue.value = declValue.value.replace(spacerValues[valueMatch], valueMatch)
+          } else {
+            problems.push({
+              index: declarationValueIndex(decl) + declValue.sourceIndex,
+              message: messages.rejected(spacerValues[valueMatch], valueMatch)
+            })
+          }
+        } else if (declValue.value !== '') {
+          problems.push({
+            index: declarationValueIndex(decl) + declValue.sourceIndex,
+            message: messages.rejected(declValue.value, null)
+          })
+        }
+      })
+
+      if (context.fix) {
+        decl.value = parsedValue.toString()
       }
-    }
-  },
-  'https://primer.style/css/support/spacing'
-)
+
+      if (containsMath) {
+        return false
+      }
+
+      if (problems.length) {
+        for (const err of problems) {
+          stylelint.utils.report({
+            index: err.index,
+            message: err.message,
+            node: decl,
+            result,
+            ruleName
+          })
+        }
+      }
+    })
+  }
+
+  return lintResult
+})
+
+function noop() {}
+
+module.exports.ruleName = ruleName
+module.exports.messages = messages
