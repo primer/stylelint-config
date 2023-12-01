60d4c45b51b2f14686f65f9cdf8966ea72df583a
langermank <18661030+langermank@users.noreply.github.com>
2023-12-01 14:50:26 -0800

    fix test to exclude things

--- plugins/new-color-vars-have-fallback.js
+++ plugins/new-color-vars-have-fallback.js
@@ -6,38 +6,25 @@ const messages = stylelint.utils.ruleMessages(ruleName, {
     `Expected a fallback value for CSS variable ${variable}. New color variables fallbacks, check primer.style/primitives to find the correct value`,
 })
 
-const fs = require('fs')
-const path = require('path')
-
-const jsonFilePath = 'node_modules/@primer/primitives/tokens-next-private/docs/functional/themes/light.json'
-let jsonContent
-
-try {
-  const fileContent = fs.readFileSync(path.resolve(jsonFilePath), 'utf8')
-  jsonContent = JSON.parse(fileContent)
-  // console.log('JSON content:', jsonContent) // Log to see the content
-} catch (error) {
-  // eslint-disable-next-line no-console
-  console.error('Error reading JSON file:', error)
-}
-
 module.exports = stylelint.createPlugin(ruleName, enabled => {
+  const variables = require('./lib/new-color-css-vars-map.json')
+
   if (!enabled) {
     return noop
   }
 
   return (root, result) => {
-    root.walkDecls(decl => {
-      for (const key of Object.keys(jsonContent)) {
-        if (decl.value.includes(`var(--${key})`)) {
+    root.walkDecls(node => {
+      for (const variable of variables) {
+        if (node.value.includes(`var(${variable})`)) {
           // Check if the declaration uses a CSS variable from the JSON
-          const match = decl.value.match(/var\(--\w+,(.*)\)/)
+          const match = node.value.match(new RegExp(`var\\(${variable},(.*)\\)`))
           if (!match || match[1].trim() === '') {
             stylelint.utils.report({
               ruleName,
               result,
-              node: decl,
-              message: messages.expectedFallback(`--${key}`),
+              node,
+              message: messages.expectedFallback(variable),
             })
           }
         }
